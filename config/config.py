383711a45f3a29188dce708962a03a18156735d5
Muhammad Umer Khan <muhammadumerk546@gmail.com>
2025-02-25 23:09:10 +0500

    Update config.py

--- config/config.py
+++ config/config.py
@@ -8,10 +8,13 @@ load_dotenv()
 # Logging configuration
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
+try:
 # API Keys (Keep them private using .env)
-GOOGLE_SEARCH_KEY = os.getenv("GOOGLE_SEARCH_API_KEY")
-SEARCH_ENGINE_ID = os.getenv("SEARCH_ENGINE_ID")
-GROQ_API_KEY = os.getenv("LANGCHAIN_GROK_API_KEY")
+    GOOGLE_SEARCH_KEY = os.getenv("GOOGLE_SEARCH_API_KEY")
+    SEARCH_ENGINE_ID = os.getenv("SEARCH_ENGINE_ID")
+    GROQ_API_KEY = os.getenv("LANGCHAIN_GROK_API_KEY")
+except:
+    logging.error("���️ API KEYS not found or not set.")
 
 # Constants
 CHUNK_SIZE = 1000
