f1954115c6b17e4a914f36308c7741727c8f67d1
Muhammad Umer Khan <muhammadumerk546@gmail.com>
2025-04-01 17:16:08 +0500

    Update vector_store.py

--- vector_db/vector_store.py
+++ vector_db/vector_store.py
@@ -4,13 +4,11 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import Document
 from config.config import CHUNK_SIZE, CHUNK_OVERLAP
+from utils.utils import configure_vector_embeddings
 import logging
 
 # Logging configuration
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
-
-# Load embedding model
-embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 def create_vector_db(texts):
     """
@@ -27,11 +25,11 @@ def create_vector_db(texts):
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP)
         documents = text_splitter.create_documents(texts)
 
-        text_embeddings = embedding_model.embed_documents([doc.page_content for doc in documents])
+        text_embeddings = configure_vector_embeddings().embed_documents([doc.page_content for doc in documents])
         faiss_docs = [Document(page_content=text) for text in texts]
 
         logging.info("✅ Vector database created successfully.")
-        return FAISS.from_documents(faiss_docs, embedding_model)
+        return FAISS.from_documents(faiss_docs, configure_vector_embeddings())
 
     except Exception as e:
         logging.error(f"❌ Error creating vector DB: {str(e)}")
